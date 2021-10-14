e15bb0f4382d6eb9d594aec3a64dbcca4099fed9
Matt Wilde <mattcwilde@gmail.com>
2021-10-14 16:21:37 -0600

    pep8 formatting

--- pyslime/fields.py
+++ pyslime/fields.py
@@ -2,8 +2,9 @@ import numpy as np
 from astropy.cosmology import Planck15
 from pyslime import utils as pu
 
+
 def spherical_velocities(slvobj):
-    xvel = slvobj.data[:,:,:,1]
+    xvel = slvobj.data[:, :, :, 1]
     yvel = slvobj.data[:, :, :, 2]
     zvel = slvobj.data[:, :, :, 3]
 
@@ -16,14 +17,14 @@ def spherical_velocities(slvobj):
     yidxs = np.arange(griddims[1])
     zidxs = np.arange(griddims[2])
 
-    xx,yy,zz = np.meshgrid(xidxs,yidxs,zidxs,indexing='ij')
+    xx, yy, zz = np.meshgrid(xidxs, yidxs, zidxs, indexing='ij')
 
-    ### Get sky coordinates at each voxel
+    # Get sky coordinates at each voxel
     print('Converting cube indices to sky coordinates...this will take a while')
-    xc,yc,zc = pu.idx_to_cartesian(xx,yy,zz,slime=slvobj)
-    ra,dec = pu.cartesian_to_sky(xc,yc,zc)
+    xc, yc, zc = pu.idx_to_cartesian(xx, yy, zz, slime=slvobj)
+    ra, dec = pu.cartesian_to_sky(xc, yc, zc)
 
-    ### Make vectors and take dot products
+    # Make vectors and take dot products
     print('Calculating components of radial vectors...')
     ra = np.radians(ra)
     dec = np.radians(dec)
@@ -31,24 +32,24 @@ def spherical_velocities(slvobj):
     radvec_y = np.sin(ra) * np.sin(90-dec)
     radvec_z = np.cos(90. - dec)
 
-    ### Get orientation vectors from data
+    # Get orientation vectors from data
     xvecs = np.float32(slvdat.data[xx, yy, zz, 1])
     yvecs = np.float32(slvdat.data[xx, yy, zz, 2])
     zvecs = np.float32(slvdat.data[xx, yy, zz, 3])
 
     dps = radvec_x * xvecs + radvec_y * yvecs + radvec_z * zvecs
 
+    return rdot, phidot, thetadot
 
-    return rdot, phidot,thetadot
 
 def normalize_velocity_vectors(slvobj):
-    xvel = slvobj.data[:,:,:,1]
+    xvel = slvobj.data[:, :, :, 1]
     yvel = slvobj.data[:, :, :, 2]
     zvel = slvobj.data[:, :, :, 3]
 
     mag = xvel**2+yvel**2+zvel**2
 
-    ### Make vectors and take dot products
+    # Make vectors and take dot products
     print('Calculating components of radial vectors...')
     ra = np.radians(ra)
     dec = np.radians(dec)
@@ -56,12 +57,11 @@ def normalize_velocity_vectors(slvobj):
     radvec_y = np.sin(ra) * np.sin(90-dec)
     radvec_z = np.cos(90. - dec)
 
-    ### Get orientation vectors from data
+    # Get orientation vectors from data
     xvecs = np.float32(slvdat.data[xx, yy, zz, 1])
     yvecs = np.float32(slvdat.data[xx, yy, zz, 2])
     zvecs = np.float32(slvdat.data[xx, yy, zz, 3])
 
     dps = radvec_x * xvecs + radvec_y * yvecs + radvec_z * zvecs
 
-
-    return rdot, phidot,thetadot
+    return rdot, phidot, thetadot
--- pyslime/slime.py
+++ pyslime/slime.py
@@ -1,8 +1,9 @@
 import numpy as np
 
+
 class Slime(object):
-    def __init__(self, type = 'trace', data=None, griddims=None, fitpars=None,
-                 name=None,physicaldims=None,physicalunit='Mpc',gridcenter=None):
+    def __init__(self, type='trace', data=None, griddims=None, fitpars=None,
+                 name=None, physicaldims=None, physicalunit='Mpc', gridcenter=None):
         self.data = data
         self.type = type
         self.griddims = griddims
@@ -12,11 +13,10 @@ class Slime(object):
         self.physicalunit = np.array(physicalunit)
         self.gridcenter = np.array(gridcenter)
 
-        ### Calculate min coords to speed up operations later on
+        # Calculate min coords to speed up operations later on
         self.calc_extreme_coords()
 
-
-    def calc_extreme_coords(self,reverse_xz=True):
+    def calc_extreme_coords(self, reverse_xz=True):
         try:
             d2cellcenter = self.physicaldims/self.griddims/2.
             halfdist = self.physicaldims/2.
@@ -41,26 +41,26 @@ class Slime(object):
             pass
 
     @classmethod
-    def from_dir(cls,dirname,datafile='trace.bin',metafile='export_metadata.txt',
-                 type='trace',axes='xyz',with_velocity=False,dtype=np.float16):
+    def from_dir(cls, dirname, datafile='trace.bin', metafile='export_metadata.txt',
+                 type='trace', axes='xyz', with_velocity=False, dtype=np.float16):
         import pyslime.utils as pu
         if dirname[-1] == '/':
             dirname = dirname[:-1]
-        ### Grab metadata 'as-is' to get dimensions of datagrid
-        rawmetadata = pu.parse_meta_file('{}{}{}'.format(dirname,'/',metafile),
-                                      axes='xyz')
-        sdata = pu.load_slime_data('{}{}{}'.format(dirname,'/', datafile),
-                                    griddims=rawmetadata['grid_res'],axes=axes,dtype=dtype,
+        # Grab metadata 'as-is' to get dimensions of datagrid
+        rawmetadata = pu.parse_meta_file('{}{}{}'.format(dirname, '/', metafile),
+                                         axes='xyz')
+        sdata = pu.load_slime_data('{}{}{}'.format(dirname, '/', datafile),
+                                   griddims=rawmetadata['grid_res'], axes=axes, dtype=dtype,
                                    with_velocity=with_velocity)
         print(np.shape(sdata))
-        ### Now grab metadata with proper ordering of axes
+        # Now grab metadata with proper ordering of axes
         metadata = pu.parse_meta_file('{}{}{}'.format(dirname, '/', metafile),
                                       axes=axes)
-        slimeobj =  cls(type=type,data=sdata,griddims=metadata['grid_res'],
-                        fitpars=metadata['fitpars'],name=dirname,
-                        physicaldims=metadata['physical_dims'],
-                        physicalunit=metadata['physical_unit'],
-                        gridcenter=metadata['grid_center'])
+        slimeobj = cls(type=type, data=sdata, griddims=metadata['grid_res'],
+                       fitpars=metadata['fitpars'], name=dirname,
+                       physicaldims=metadata['physical_dims'],
+                       physicalunit=metadata['physical_unit'],
+                       gridcenter=metadata['grid_center'])
         return slimeobj
 
     def cartesian_to_idx(self, x, y, z):
@@ -84,34 +84,34 @@ class Slime(object):
         if self.mincoords[0] < self.maxcoords[0]:
             i = (x - self.mincoords[0]) / scales[0]
         else:
-            i =(self.mincoords[0]-x) / scales[0]
+            i = (self.mincoords[0]-x) / scales[0]
         if self.mincoords[1] < self.maxcoords[1]:
             j = (y - self.mincoords[1]) / scales[1]
         else:
-            j =(self.mincoords[1]-y) / scales[1]
+            j = (self.mincoords[1]-y) / scales[1]
         if self.mincoords[2] < self.maxcoords[2]:
             k = (z - self.mincoords[2]) / scales[2]
         else:
-            k =(self.mincoords[2]-z) / scales[2]
+            k = (self.mincoords[2]-z) / scales[2]
         i = np.rint(i).astype(int)
         j = np.rint(j).astype(int)
         k = np.rint(k).astype(int)
 
         return i, j, k
 
-    def sky_to_data(self,ra,dec,redshift,check_bounds=False,veldim=None):
+    def sky_to_data(self, ra, dec, redshift, check_bounds=False, veldim=None):
         import pyslime.utils as pu
         cart = pu.transform_to_cartesian(ra, dec, redshift)
         idxs = self.cartesian_to_idx(cart[0], cart[1], cart[2])
-        is4d = len(np.shape(self.data))==4
+        is4d = len(np.shape(self.data)) == 4
         if check_bounds:
-            inbounds = ((idxs[0]>0)&(idxs[1]>0)&(idxs[2]>0)&
-                        (idxs[0]<self.griddims[0])&(idxs[1]<self.griddims[1])&
-                        (idxs[2]<self.griddims[2]))
+            inbounds = ((idxs[0] > 0) & (idxs[1] > 0) & (idxs[2] > 0) &
+                        (idxs[0] < self.griddims[0]) & (idxs[1] < self.griddims[1]) &
+                        (idxs[2] < self.griddims[2]))
             toreturn = np.zeros_like(idxs[0])-642.
-            if (veldim not in [1,2,3])&is4d:
-                toreturn[inbounds] = self.data[idxs[0][inbounds],idxs[1][inbounds],
-                idxs[2][inbounds],0]
+            if (veldim not in [1, 2, 3]) & is4d:
+                toreturn[inbounds] = self.data[idxs[0][inbounds], idxs[1][inbounds],
+                                               idxs[2][inbounds], 0]
             elif is4d:
                 toreturn[inbounds] = self.data[idxs[0][inbounds], idxs[1][inbounds],
                                                idxs[2][inbounds], veldim]
@@ -120,7 +120,7 @@ class Slime(object):
                                                idxs[2][inbounds]]
         else:
             if (veldim not in [1, 2, 3]) & is4d:
-                toreturn = self.data[idxs[0], idxs[1], idxs[2],0]
+                toreturn = self.data[idxs[0], idxs[1], idxs[2], 0]
             elif is4d:
                 toreturn = self.data[idxs[0], idxs[1], idxs[2], veldim]
             else:
@@ -128,33 +128,36 @@ class Slime(object):
 
         return toreturn
 
-    def standardize(self,mean=None,stddev=None):
+    def standardize(self, mean=None, stddev=None):
         randvals = self.random_sample(100000)
         if mean is None:
             mean = np.mean(randvals[~np.isneginf(randvals)])
         if stddev is None:
-            stddev = np.std(randvals[~np.isneginf(randvals)],dtype=np.float32)
+            stddev = np.std(randvals[~np.isneginf(randvals)], dtype=np.float32)
         self.data = (self.data - mean) / stddev
 
-    def random_sample(self,size=10000, velocities=False):
+    def random_sample(self, size=10000, velocities=False):
         import pyslime.utils as pu
-        randvals = pu.sample_cube(self.data,size,velocities=velocities)
+        randvals = pu.sample_cube(self.data, size, velocities=velocities)
         return randvals
 
-    def slice3d(self,coords,size = [10.,10.,10.],skycoords=True):
+    def slice3d(self, coords, size=[10., 10., 10.], skycoords=True):
         import pyslime.utils as pu
         if skycoords:
-            cartcoords = pu.transform_to_cartesian(coords[0],coords[1],coords[2])
+            cartcoords = pu.transform_to_cartesian(
+                coords[0], coords[1], coords[2])
         else:
             cartcoords = coords
         startcoords = [(cartcoords[i] - size[i]/2.) for i in range(3)]
         endcoords = [(cartcoords[i] + size[i]/2.) for i in range(3)]
-        startidxs  = self.cartesian_to_idx(startcoords[0],startcoords[1],startcoords[2])
-        endidxs = self.cartesian_to_idx(endcoords[0], endcoords[1], endcoords[2])
+        startidxs = self.cartesian_to_idx(
+            startcoords[0], startcoords[1], startcoords[2])
+        endidxs = self.cartesian_to_idx(
+            endcoords[0], endcoords[1], endcoords[2])
 
         sliceobj = self.copy()
-        sliceobj.data = self.data[startidxs[0]:endidxs[0],startidxs[1]:endidxs[1],
-                   startidxs[2]:endidxs[2]]
+        sliceobj.data = self.data[startidxs[0]:endidxs[0], startidxs[1]:endidxs[1],
+                                  startidxs[2]:endidxs[2]]
         sliceobj.gridcenter = cartcoords
         sliceobj.physicaldims = np.array(size)
         sliceobj.mincoords = startcoords
@@ -162,10 +165,9 @@ class Slime(object):
         sliceobj.griddims = np.shape(sliceobj.data)
         return sliceobj
 
-
     def copy(self):
-        newobj = Slime(data=self.data,griddims=self.griddims,fitpars=self.fitpars,
-                       name=self.name,physicaldims=self.physicaldims,
-                       physicalunit=self.physicalunit,gridcenter=self.gridcenter,
+        newobj = Slime(data=self.data, griddims=self.griddims, fitpars=self.fitpars,
+                       name=self.name, physicaldims=self.physicaldims,
+                       physicalunit=self.physicalunit, gridcenter=self.gridcenter,
                        type=self.type)
         return newobj
