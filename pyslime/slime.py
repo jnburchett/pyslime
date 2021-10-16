8229b16eddc3e96a883d97594be6173238856121
Matt Wilde <mattcwilde@gmail.com>
2021-10-16 12:21:56 -0600

    formatting

--- pyslime/slime.py
+++ pyslime/slime.py
@@ -2,8 +2,17 @@ import numpy as np
 
 
 class Slime(object):
-    def __init__(self, type='trace', data=None, griddims=None, fitpars=None,
-                 name=None, physicaldims=None, physicalunit='Mpc', gridcenter=None):
+    def __init__(
+        self,
+        type="trace",
+        data=None,
+        griddims=None,
+        fitpars=None,
+        name=None,
+        physicaldims=None,
+        physicalunit="Mpc",
+        gridcenter=None,
+    ):
         self.data = data
         self.type = type
         self.griddims = griddims
@@ -18,14 +27,14 @@ class Slime(object):
 
     def calc_extreme_coords(self, reverse_xz=True):
         try:
-            d2cellcenter = self.physicaldims/self.griddims/2.
-            halfdist = self.physicaldims/2.
+            d2cellcenter = self.physicaldims / self.griddims / 2.0
+            halfdist = self.physicaldims / 2.0
             mincoords = self.gridcenter - halfdist + d2cellcenter
             self.mincoords = mincoords
             maxcoords = self.gridcenter + halfdist - d2cellcenter
             self.maxcoords = maxcoords
 
-            '''
+            """
             mincoo = mincoords.copy()
             maxcoo = maxcoords.copy()
             if reverse_xz:
@@ -35,36 +44,56 @@ class Slime(object):
                 #self.mincoords[1] = maxcoo[1]
                 self.maxcoords[0] = mincoo[0]
                 self.mincoords[0] = maxcoo[0]
-                pass'''
+                pass"""
 
         except:
             pass
 
     @classmethod
-    def from_dir(cls, dirname, datafile='trace.bin', metafile='export_metadata.txt',
-                 type='trace', axes='xyz', with_velocity=False, dtype=np.float16):
+    def from_dir(
+        cls,
+        dirname,
+        datafile="trace.bin",
+        metafile="export_metadata.txt",
+        type="trace",
+        axes="xyz",
+        with_velocity=False,
+        dtype=np.float16,
+    ):
         import pyslime.utils as pu
-        if dirname[-1] == '/':
+
+        if dirname[-1] == "/":
             dirname = dirname[:-1]
         # Grab metadata 'as-is' to get dimensions of datagrid
-        rawmetadata = pu.parse_meta_file('{}{}{}'.format(dirname, '/', metafile),
-                                         axes='xyz')
-        sdata = pu.load_slime_data('{}{}{}'.format(dirname, '/', datafile),
-                                   griddims=rawmetadata['grid_res'], axes=axes, dtype=dtype,
-                                   with_velocity=with_velocity)
+        rawmetadata = pu.parse_meta_file(
+            "{}{}{}".format(dirname, "/", metafile), axes="xyz"
+        )
+        sdata = pu.load_slime_data(
+            "{}{}{}".format(dirname, "/", datafile),
+            griddims=rawmetadata["grid_res"],
+            axes=axes,
+            dtype=dtype,
+            with_velocity=with_velocity,
+        )
         print(np.shape(sdata))
         # Now grab metadata with proper ordering of axes
-        metadata = pu.parse_meta_file('{}{}{}'.format(dirname, '/', metafile),
-                                      axes=axes)
-        slimeobj = cls(type=type, data=sdata, griddims=metadata['grid_res'],
-                       fitpars=metadata['fitpars'], name=dirname,
-                       physicaldims=metadata['physical_dims'],
-                       physicalunit=metadata['physical_unit'],
-                       gridcenter=metadata['grid_center'])
+        metadata = pu.parse_meta_file(
+            "{}{}{}".format(dirname, "/", metafile), axes=axes
+        )
+        slimeobj = cls(
+            type=type,
+            data=sdata,
+            griddims=metadata["grid_res"],
+            fitpars=metadata["fitpars"],
+            name=dirname,
+            physicaldims=metadata["physical_dims"],
+            physicalunit=metadata["physical_unit"],
+            gridcenter=metadata["grid_center"],
+        )
         return slimeobj
 
     def cartesian_to_idx(self, x, y, z):
-        '''Transform coordinates from physical space to corresponding indices
+        """Transform coordinates from physical space to corresponding indices
         in model data grid
 
         Parameters
@@ -78,21 +107,21 @@ class Slime(object):
         -------
         i,j,k : floats or arrays
             Indices into slimeobj.data
-        '''
-        scales = self.physicaldims/self.griddims
+        """
+        scales = self.physicaldims / self.griddims
 
         if self.mincoords[0] < self.maxcoords[0]:
             i = (x - self.mincoords[0]) / scales[0]
         else:
-            i = (self.mincoords[0]-x) / scales[0]
+            i = (self.mincoords[0] - x) / scales[0]
         if self.mincoords[1] < self.maxcoords[1]:
             j = (y - self.mincoords[1]) / scales[1]
         else:
-            j = (self.mincoords[1]-y) / scales[1]
+            j = (self.mincoords[1] - y) / scales[1]
         if self.mincoords[2] < self.maxcoords[2]:
             k = (z - self.mincoords[2]) / scales[2]
         else:
-            k = (self.mincoords[2]-z) / scales[2]
+            k = (self.mincoords[2] - z) / scales[2]
         i = np.rint(i).astype(int)
         j = np.rint(j).astype(int)
         k = np.rint(k).astype(int)
@@ -101,23 +130,32 @@ class Slime(object):
 
     def sky_to_data(self, ra, dec, redshift, check_bounds=False, veldim=None):
         import pyslime.utils as pu
+
         cart = pu.transform_to_cartesian(ra, dec, redshift)
         idxs = self.cartesian_to_idx(cart[0], cart[1], cart[2])
         is4d = len(np.shape(self.data)) == 4
         if check_bounds:
-            inbounds = ((idxs[0] > 0) & (idxs[1] > 0) & (idxs[2] > 0) &
-                        (idxs[0] < self.griddims[0]) & (idxs[1] < self.griddims[1]) &
-                        (idxs[2] < self.griddims[2]))
-            toreturn = np.zeros_like(idxs[0])-642.
+            inbounds = (
+                (idxs[0] > 0)
+                & (idxs[1] > 0)
+                & (idxs[2] > 0)
+                & (idxs[0] < self.griddims[0])
+                & (idxs[1] < self.griddims[1])
+                & (idxs[2] < self.griddims[2])
+            )
+            toreturn = np.zeros_like(idxs[0]) - 642.0
             if (veldim not in [1, 2, 3]) & is4d:
-                toreturn[inbounds] = self.data[idxs[0][inbounds], idxs[1][inbounds],
-                                               idxs[2][inbounds], 0]
+                toreturn[inbounds] = self.data[
+                    idxs[0][inbounds], idxs[1][inbounds], idxs[2][inbounds], 0
+                ]
             elif is4d:
-                toreturn[inbounds] = self.data[idxs[0][inbounds], idxs[1][inbounds],
-                                               idxs[2][inbounds], veldim]
+                toreturn[inbounds] = self.data[
+                    idxs[0][inbounds], idxs[1][inbounds], idxs[2][inbounds], veldim
+                ]
             else:
-                toreturn[inbounds] = self.data[idxs[0][inbounds], idxs[1][inbounds],
-                                               idxs[2][inbounds]]
+                toreturn[inbounds] = self.data[
+                    idxs[0][inbounds], idxs[1][inbounds], idxs[2][inbounds]
+                ]
         else:
             if (veldim not in [1, 2, 3]) & is4d:
                 toreturn = self.data[idxs[0], idxs[1], idxs[2], 0]
@@ -128,7 +166,7 @@ class Slime(object):
 
         return toreturn
 
-    def standardize(self, mean=None, stddev=None, denscut=None):
+    def standardize(self, denscut=None):
         """ Standardize the distribution by subtracting by the mean 
         and divitding by the standard deviation of the distribuiton of
         density. This assumes the density is already in log10 space. Can 
@@ -141,14 +179,12 @@ class Slime(object):
             denscut (float, optional): an upper limit on the density to
                 calculate the mean and std. Defaults to -9999.
         """
+        from itertools import product
+        import scipy.stats
 
         randvals = self.random_sample(1000000)
         if denscut is None:
-            if mean is None:
-                mean = np.mean(randvals[~np.isneginf(randvals)])
-            if stddev is None:
-                stddev = np.std(randvals[~np.isneginf(randvals)],
-                                dtype=np.float32)
+            pass
 
         else:
             cut = randvals > denscut
@@ -161,26 +197,30 @@ class Slime(object):
 
     def random_sample(self, size=10000, velocities=False):
         import pyslime.utils as pu
+
         randvals = pu.sample_cube(self.data, size, velocities=velocities)
         return randvals
 
-    def slice3d(self, coords, size=[10., 10., 10.], skycoords=True):
+    def slice3d(self, coords, size=[10.0, 10.0, 10.0], skycoords=True):
         import pyslime.utils as pu
+
         if skycoords:
-            cartcoords = pu.transform_to_cartesian(
-                coords[0], coords[1], coords[2])
+            cartcoords = pu.transform_to_cartesian(coords[0], coords[1], coords[2])
         else:
             cartcoords = coords
-        startcoords = [(cartcoords[i] - size[i]/2.) for i in range(3)]
-        endcoords = [(cartcoords[i] + size[i]/2.) for i in range(3)]
+        startcoords = [(cartcoords[i] - size[i] / 2.0) for i in range(3)]
+        endcoords = [(cartcoords[i] + size[i] / 2.0) for i in range(3)]
         startidxs = self.cartesian_to_idx(
-            startcoords[0], startcoords[1], startcoords[2])
-        endidxs = self.cartesian_to_idx(
-            endcoords[0], endcoords[1], endcoords[2])
+            startcoords[0], startcoords[1], startcoords[2]
+        )
+        endidxs = self.cartesian_to_idx(endcoords[0], endcoords[1], endcoords[2])
 
         sliceobj = self.copy()
-        sliceobj.data = self.data[startidxs[0]:endidxs[0], startidxs[1]:endidxs[1],
-                                  startidxs[2]:endidxs[2]]
+        sliceobj.data = self.data[
+            startidxs[0] : endidxs[0],
+            startidxs[1] : endidxs[1],
+            startidxs[2] : endidxs[2],
+        ]
         sliceobj.gridcenter = cartcoords
         sliceobj.physicaldims = np.array(size)
         sliceobj.mincoords = startcoords
@@ -189,8 +229,14 @@ class Slime(object):
         return sliceobj
 
     def copy(self):
-        newobj = Slime(data=self.data, griddims=self.griddims, fitpars=self.fitpars,
-                       name=self.name, physicaldims=self.physicaldims,
-                       physicalunit=self.physicalunit, gridcenter=self.gridcenter,
-                       type=self.type)
+        newobj = Slime(
+            data=self.data,
+            griddims=self.griddims,
+            fitpars=self.fitpars,
+            name=self.name,
+            physicaldims=self.physicaldims,
+            physicalunit=self.physicalunit,
+            gridcenter=self.gridcenter,
+            type=self.type,
+        )
         return newobj
